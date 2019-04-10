dba3f00eff7d6d1ff43a2ddad9a4d2a7c254ddbf
Administrator <Administrator@pc-03>
2019-04-10 15:51:38 +0900

    1st commit - gitdemo prj

---
+++ gitdemo/src/org/study/demo/GitDemo.java
@@ -0,0 +1,10 @@
+package org.study.demo;
+
+public class GitDemo {
+
+	public static void main(String[] args) {
+		// 이 파일이 깃허브로 업로드 됩니다.
+
+	}
+
+}
---
+++ gitdemo/src/org/study/demo/MyMainApp.java
@@ -0,0 +1,10 @@
+package org.study.demo;
+
+public class MyMainApp {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+
+	}
+
+}
